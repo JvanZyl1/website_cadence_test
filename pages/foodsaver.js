6a94e89e9b82f63de9384bdb7576e6f53ff65c25
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2024-07-25 08:00:50 +0530

    'yoo'

--- pages/foodsaver.js
+++ pages/foodsaver.js
@@ -1,6 +1,7 @@
 // Import React and any other libraries you need
 import React from 'react';
 import Head from 'next/head'; // For handling everything in the <head> of the HTML document
+import Image from 'next/image'; // Import the Image component
 
 // Define the functional component
 const FoodSaver = () => {
@@ -12,8 +13,15 @@ const FoodSaver = () => {
       </Head>
       <h1>Welcome to Food Saver</h1>
       <p>Helping you save food and the environment with easy, practical solutions.</p>
-      <img src="/images/your-image.jpg" alt="Food Saving Example" style={{ width: "100%", height: "auto" }} />
-      {/* Replace '/images/your-image.jpg' with the path to an actual image in your public/images directory */}
+      <div style={{ position: 'relative', width: '100%', height: 'auto' }}>
+        <Image
+          src="/Images/Jamie_Vardy_2018.jpg" // Ensure the path is correct
+          alt="Food Saving Example"
+          layout="fill" // This makes the image fill the container
+          objectFit="cover" // Adjust the fitting of the image to cover the area of the container
+        />
+      </div>
+      {/* Note: Adjust the container style as needed to ensure it has a defined height when using layout='fill' */}
     </div>
   );
 };
